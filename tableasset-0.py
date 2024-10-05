b3ad463cae330de4caa62fdc6fa857f43de00b82
hubbert Smith <52464327+hubbertsmith@users.noreply.github.com>
2024-10-05 09:40:59 -0600

    Update tableasset-0.py

--- tableasset-0.py
+++ tableasset-0.py
@@ -6,8 +6,8 @@ from psycopg2 import sql
 db_params = {
     'dbname': 'i4catalog-v1',
     'user': 'hubbert',
-    'password': 'u5',
-    'host': 'u5',
+    'password': 'u0',
+    'host': 'u0',
     'port': 5432
 }
 
